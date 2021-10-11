d3f9a9c2ac6a50af619224810e07f7327854f638
Ivo196 <ivo.tonioni@gmail.com>
2021-10-11 01:11:18 -0300

    Create Curruncy Converter.js

---
+++ Currency Converter/Curruncy Converter.js
@@ -0,0 +1,10 @@
+function main() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+	function convert(num1,num2) {
+		let prod =num1*num2;
+		return prod
+	}
+	let res= convert (amount,rate);
+	console.log(res)
+}
